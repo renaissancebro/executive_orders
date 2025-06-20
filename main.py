4190058891e7353fd0460362da5b85da43cbcbbe
josh <jcfreeman23@gmail.com>
2025-06-20 09:45:28 -0500

    Moved git ignore

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from modules.check import check_last_policy
 from modules.scraper import grab_meta_data
 from modules.emailer import make_email, send_mail
+from fake_entry import insert_dummy
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'alert_tripper')))
 
 def main():
+    
     if check_last_policy():  # True = new post
         meta = grab_meta_data()
         subject, body = make_email(meta)
